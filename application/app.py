a492e6eb43843e7310a9ab7359019a48b33e010a
JosephCVA <jalhambra@sfsu.edu>
2025-02-21 15:49:14 -0800

    about page

--- application/app.py
+++ application/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/joseph")
+def joseph():
+    return render_template('about_JosephA.html')
+
 if __name__ == '__main__':
     app.run()
