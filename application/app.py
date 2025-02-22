c5543c2f0a1fafac14981256a71c85deef537e7b
Hluii <lui_hilary@yahoo.com>
2025-02-22 13:39:35 -0800

    Hilary's about page done

--- application/app.py
+++ application/app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+
+@app.route("/hilary")
+def hilary():
+    return render_template('hilary.html')
+
 if __name__ == '__main__':
     app.run()
