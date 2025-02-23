05058b3b07d0beaf863f07e5b8753b94d33d87aa
Sid Padmanabhuni <siddharth.padm@gmail.com>
2025-02-23 07:47:52 +0000

    sid's about me

--- application/app.py
+++ application/app.py
@@ -10,5 +10,9 @@ def index():
 def annison():
     return render_template('annison.html')
 
+@app.route("/sid")
+def sid():
+    return render_template('sid.html')
+
 if __name__ == '__main__':
     app.run()
