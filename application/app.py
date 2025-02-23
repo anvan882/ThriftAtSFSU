e9f766529bff1b1d0e9a06b050b3086a10a52d68
joseph-shur <jshur1@sfsu.edu>
2025-02-23 14:27:30 -0800

    re-added DS-Store to gitignore and corrected file path for img

--- application/app.py
+++ application/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/joe")
+def joe():
+    return render_template('joe.html') 
+
 if __name__ == '__main__':
     app.run()
